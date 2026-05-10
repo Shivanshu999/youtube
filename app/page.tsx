4e521a286177a22ce81711be3f1faddaa053fbbd
Shivanshu999 <shivanshuawasthi4@gmail.com>
2026-05-10 22:23:20 +0530

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+import SignIn from "./components/sign-in";
+
+export default async function Home() {
+  const session = await auth();
+
+  // User logged in
+  if (session?.user) {
+    redirect("/feed");
+  }
+
+  // User not logged in
   return (
-    <div>
-      hi
-    </div>
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold">
+          Welcome to YouTube Clone
+        </h1>
+
+        <SignIn />
+      </div>
+    </main>
   );
 }
